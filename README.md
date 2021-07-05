fa22aa04cef91899f24f14bf2f27b0208d428405
jclarson <jclarson@hotmail.com>
2021-07-05 17:10:43 -0500

    adds the files to the repo

---
+++ README.md
@@ -0,0 +1,28 @@
+# Name:
+
+Projectile
+
+#Description:
+
+The goal of this page was to generate balls in random locations, and have them move within a bounded area, affected by gravity.
+I also opted to make the ball color be randomly generated.
+
+# Installation:
+
+Requires the index.html file, projectileball.js and projectilestyle.css files to be in the same directory.
+
+# Usage:
+
+Load the HTML file in a browser to see the balls.
+
+# Support:
+
+No support is provided for this application.  Use at your own discretion.
+
+# Roadmap:
+
+No future options are planned for this project at this time.
+
+# License information:
+
+No licensing is required by the author for use of this project.
---
+++ projectileball.js
@@ -0,0 +1,68 @@
+const size = 30; // size of ball
+const balls = []; // array to hold all free balls
+let gravity = 1;
+
+// this function makes a ball with position and velocity set
+function makeBall(xcoord, ycoord, color, velx = 0, vely = 0, fixed = 0) {
+  let statObj = {x: xcoord, y: ycoord, velx: velx, vely: vely};
+  ball = document.createElement("div");
+  ball.style.backgroundColor = color;
+  ball.className = "ball";
+  ball.style.height = ball.style.width = size;
+  ball.style.top = ycoord;
+  ball.style.left = xcoord;
+  document.body.appendChild(ball);
+  
+  if (!fixed) {
+    // only free balls will be updated
+    balls.push([ball,statObj]);
+  }
+}
+
+function checkWalls(stat) {
+  if (stat.x < 0) {
+    stat.velx = -stat.velx;
+    stat.x = 0;
+  } else if (stat.x > (800 - size)) {
+    stat.velx = -stat.velx;
+    stat.x = 800 - size;
+  }
+
+  if (stat.y < 0) {
+    stat.y = 0;
+  } else if (stat.y > (400 - size)) {
+    stat.vely = -stat.vely;
+    stat.y = 400 - size;
+  }
+}
+
+function update() {
+  for (let i = 0; i < balls.length; i++) {
+    let stat = balls[i][1];
+    stat.x += stat.velx;
+    stat.vely += gravity;
+    stat.y += stat.vely;
+    checkWalls(stat);
+
+    balls[i][0].style.left = stat.x
+    balls[i][0].style.top = stat.y
+  }
+  setTimeout(update, 100); // this calls update every 1/10 second
+}
+
+function getRandom(scale) {
+  return Math.random() * scale;
+}
+
+function factory(total) {
+  // check how many balls exist already and add to the array
+  for (let i = balls.length; i < total; i++) {
+    let velx = Math.floor(getRandom(20) - 10);
+    let vely = Math.floor(getRandom(20) - 10);
+    let colorNumber = Math.floor(getRandom(16777215));
+    makeBall(getRandom(800 - size), getRandom(400 - size), colorNumber.toString(16), velx, vely, 0);
+  }
+  update();
+}
+
+factory(10);
